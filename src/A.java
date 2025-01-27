b6635a15e299ed6b13c061a389ae96b7db1700da
jnsheril <jnsheril@gmail.com>
2025-01-27 21:10:55 +0530

    SecondCommit

---
+++ src/A.java
@@ -0,0 +1,8 @@
+public class A{
+    public static void main(String[] args) {
+        //123
+       
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/B.java
@@ -0,0 +1,7 @@
+public class B {
+    public static void main(String[] args) {
+        //121
+        System.out.println("heloo users");
+    }
+    
+}
